7a15fca4bd5e9f37d2bb7035f4f4261f173b3b80
Yogesh Seetharaman <yogeshs@umich.edu>
2014-09-06 20:53:16 -0400

    fixed

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,47 +1,73 @@
 $(document).ready(function() {
-	//var serverURL = "ec2-54-68-38-168.us-west-2.compute.amazonaws.com:8080";
-	var serverURL = "localhost:8080";
-	function getAJAX() {
-		$.ajax({
-			contentType: 'application/json',
-			type: "GET",
-			dataType: "json",
-			url: "http://" + serverURL + "/api/comments/?url=" + document.URL,
 
-        success: function(data) {
-          console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                var id = '#' + data[i]['_id'];
-                $("body").append($('<div id="' + data[i]['_id'] + '"/>')).css({
-                    left: data[i]['position'][0],
-                    top: data[i]['position'][1]
-                });
-                $(id).after().html('<div class="comment" style="left:' + 
-                    data[i]['position'][0] + 'px; top:' + data[i]['position'][1] + 'px; position:absolute;">' +
-                    '<a href="javascript:void(0)" class="upvote">▲</a>' +
-            		'<a href="javascript:void(0)" class="downvote">▼</a>' + data[i]['text'] + '</div>');
-            };
-        }
-    });
+    var serverURL = "ec2-54-68-38-168.us-west-2.compute.amazonaws.com:8080";
+    var docUrl = document.URL;
+    if (docUrl[docUrl.length - 1] != '/') docUrl = docUrl + '/'
+    //var serverURL = "localhost:8080";
+    function getAJAX() {
+        $.ajax({
+            contentType: 'application/json',
+            type: "GET",
+            dataType: "json",
+            url: "http://" + serverURL + "/api/comments/?url=" + docUrl,
+
+            success: function(data) {
+              console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    var id = '#' + data[i]['_id'];
+                    $("body").append($('<div id="' + data[i]['_id'] + '"/>')).css({
+                        left: data[i]['position'][0],
+                        top: data[i]['position'][1]
+                    });
+                    $(id).after().html('<div class="comment" style="left:' +
+                        data[i]['position'][0] + 'px; top:' + data[i]['position'][1] + 'px; position:absolute;">' +
+                        '<a href="javascript:void(0)" class="upvote">▲</a>' +
+                        '<a href="javascript:void(0)" class="downvote">▼</a>' + data[i]['text'] + '</div>');
+                };
+            }
+        });
     }
     getAJAX();
 
-    $(".upvote").click(function() {
-    	console.log('accessed up');
+    function postAJAX(server_id, vote) {
+        $.ajax({
+            type: "POST",
+            data: { "id": server_id, "vote": vote },
+            url: "http://" + serverURL + "/api/comments/?url=" + docUrl,
+
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(e) {
+                console.log(e);
+            }
+        });
+    }
+    $("body").on("click", ".upvote", function() {
+        console.log('upvote');
+        var server_id = $(this).parent("div").parent().attr('id');
+        postAJAX(server_id, 1);
+        return false;
+    });
+    $("body").on("click", ".downvote", function() {
+        console.log('upvote');
+        var server_id = $(this).parent("div").parent().attr('id');
+        postAJAX(server_id, -1);
+        return false;
     });
 
     var count = 0;
     $("body").click(function(e) {
         // find the position in the current window
         var wrapper = $(this).parent();
-        var parentOffset = wrapper.offset(); 
+        var parentOffset = wrapper.offset();
         var relX = e.pageX - parentOffset.left + wrapper.scrollLeft();
         var relY = e.pageY - parentOffset.top + wrapper.scrollTop();
         // add a div here
         $(this).append($('<div/>').addClass('placeddiv').css({
             left: relX,
             top: relY,
-        }));    
+        }));
         var current = $('.placeddiv');
         console.log(current);
         $('.placeddiv').after().html('<form class="tsukform">' +
@@ -49,7 +75,7 @@ $(document).ready(function() {
             '<input type="submit" style="display: none;"></form>');
         $('.placeddiv').css("position", "absolute");
         /*
-        $('.placeddiv').after().html('<input class="tsukomi" type="text" name="textbox' + '" value="" id="t' + count + '" />' + 
+        $('.placeddiv').after().html('<input class="tsukomi" type="text" name="textbox' + '" value="" id="t' + count + '" />' +
             '<span id="tsuktxt-' + count + '"></span><input type="submit" style="display: none;">');
         */
         $('input').keypress(function (e) {
@@ -59,23 +85,23 @@ $(document).ready(function() {
                     type: "PUT",
                     data: comment,
 
-					url: "http://" + serverURL + "/api/comments/?url=" + document.URL,
+                    url: "http://" + serverURL + "/api/comments/?url=" + docUrl,
 
-					success: function(data) {
-						console.log(data);
-					},
-					error: function(e) {
-						console.log(e);
-					}
-				});
-				current.css('display', 'none');
-				$('input').css('display', 'none');
-				getAJAX();
-				//$('#t0').val('save');
-				return false;    //<---- Add this line
-			}
-		});
-		$(this).unbind('click');
-		count = count + 1;
-	});
+                    success: function(data) {
+                        console.log(data);
+                    },
+                    error: function(e) {
+                        console.log(e);
+                    }
+                });
+                current.css('display', 'none');
+                $('input').css('display', 'none');
+                getAJAX();
+                //$('#t0').val('save');
+                return false;    //<---- Add this line
+            }
+        });
+        $(this).unbind('click');
+        count = count + 1;
+    });
 });
